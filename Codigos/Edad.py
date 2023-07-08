fe2f0e179dbc7a8c01bd9dd88416b2088d8c3428
Benji <benjifran27@gmail.com>
2023-07-07 20:49:47 -0600

    Edad

---
+++ Codigos/Edad.py
@@ -0,0 +1,7 @@
+year = 2023
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu primer apellido: ")
+edad = input("Ingresa tu edad: ")
+born_year = year - int(edad)
+
+print(f'Hola {nombre} {apellido}, naciste en {born_year}')
